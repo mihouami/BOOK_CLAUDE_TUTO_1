2d4412b96d3879ab4578873e9008a0658ea43bca
Minou <am.mihoubi@gmail.com>
2024-12-30 17:05:55 +0100

    Step 2: Creating our Book Model and setting up the database

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published_year', 'created_at')
+    list_filter = ('author', 'published_year')
+    search_fields = ('title', 'author')
+    ordering = ('-created_at',)
+
--- books/models.py
+++ books/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    description = models.TextField()
+    published_year = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class meta:
+        ordering = ['-created_at']
+    
+    def __str__(self):
+        return f'{self.title} by {self.author}'
